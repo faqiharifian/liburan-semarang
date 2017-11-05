af7dea7d4095df0e10d245efeef9070afa2b08ff
Faqih Arifian <faqiharifianaji@gmail.com>
2017-11-05 09:54:46 +0700

    double back to exit

--- app/src/main/java/com/arifian/training/liburansemarang/MainActivity.kt
+++ app/src/main/java/com/arifian/training/liburansemarang/MainActivity.kt
@@ -3,10 +3,12 @@ package com.arifian.training.liburansemarang
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.arifian.training.liburansemarang.Utils.PreferenceUtils
 import com.arifian.training.liburansemarang.databinding.ActivityMainBinding
 
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var mBinding: ActivityMainBinding
 
     lateinit var pref: PreferenceUtils
+    var doubleBackToExitPressedOnce: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +33,17 @@ class MainActivity : AppCompatActivity() {
         mBinding.fab.setOnClickListener{
             startActivity(Intent(this, AddActivity::class.java))
         }
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+        }
+
+        doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
     }
 
     class ViewPagerAdapter(val fragmentManager: FragmentManager): FragmentStatePagerAdapter(fragmentManager){
